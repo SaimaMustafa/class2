3477f473549f399ced204e7bc89b8aaf18cd8f77
SaimaMustafa <SaimaMustafa7878@github.com>
2020-12-02 15:48:31 +0500

    text removed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello from Saima Mustafa.
+         Hello .
         </p>
         <a
           className="App-link"

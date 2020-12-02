4c1832a94e20a33123be436e65d62bcc465ca4bf
SaimaMustafa <SaimaMustafa7878@github.com>
2020-12-02 20:00:02 +0500

    logo removed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         this is a sample line .
-        </p>
+      <header  >
+      <h1>Hello</h1>
+        
+      
         
       </header>
     </div>

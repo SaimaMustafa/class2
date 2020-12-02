fc59947ea2460a2eed3754b7899284363142264a
SaimaMustafa <SaimaMustafa7878@github.com>
2020-12-02 21:11:44 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
          Hello from Saima Mustafa.
         </p>
